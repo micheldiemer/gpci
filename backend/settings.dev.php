a3d14cfea427d530c8baf9cbf6f9c5811ef51daf
AbdouVak <abdou.vakouiev@gmail.com>
2023-10-19 15:36:21 +0200

    ajout de la connection a la bdd

---
+++ backend/settings.dev.php
@@ -0,0 +1,19 @@
+<?php
+
+$settings = array(
+    'driver' => 'mysql',
+    'host' => '127.0.0.1',
+    'database' => 'gpci',
+    'username' => 'root',
+    'password' => 'sql',
+    'collation' => 'utf8_general_ci',
+    'charset' => 'utf8',
+    'prefix' => ''
+);
+
+$smtpSettings = array(
+    'MAIL_HOST' => '192.168.1.30',
+    'MAIL_PORT' => 25,
+    'MAIL_USERNAME' => 'mailuser@192.168.1.30',
+    'MAIL_PASSWORD' => 'mailuser'
+);
